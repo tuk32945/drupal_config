a68e887dc48e92b6a4944141ce810ed15cf9c34b
Frank Mullings <tuk32945@temple.du>
2018-07-16 10:11:37 -0400

    2nd commit

---
+++ sql_dump/empty_table_db_for_new_installs.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 10, 2018 at 10:13 AM
+-- Server version: 5.7.22
+-- PHP Version: 7.1.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uklein`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `empty_table`
+--
+
+CREATE TABLE `empty_table` (
+  `1` int(5) NOT NULL,
+  `2` int(5) NOT NULL,
+  `4` int(5) NOT NULL,
+  `5` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
